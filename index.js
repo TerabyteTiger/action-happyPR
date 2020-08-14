1cfbe6c82506e79439b0983c509fd27320c1a1a1
Tyler V <16465776+TerabyteTiger@users.noreply.github.com>
2020-08-14 14:43:40 -0400

    🍀 Hope this works

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const Sentiment = require("sentiment");
 
 try {
   let mood = new Sentiment();
-  let result = mood.analyze("test memo! :D");
-  console.log(`Hello ${result}!`);
+  let result = mood.analyze(github.context.payload.pull_request.body);
+  console.log(`PR Description: ${github.context.payload.pull_request.body}!`);
+  console.log(`Analysis: ${result.score}`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
